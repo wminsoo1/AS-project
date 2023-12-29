94fc261b1c1d407bcf45bf2560bfa34cb9c4a58d
wminsoo1 <wminsoo1@naver.com>
2023-12-29 17:27:37 +0900

    modify main

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
 import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         numButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), NumActivity.class);
+                Intent intent=new Intent(getApplicationContext(), LinkNumActivity.class);
                 startActivity(intent);
             }
         });
