0a625a9ce17e49fc0353766c8a1f83eaec2a429f
wminsoo1 <wminsoo1@naver.com>
2023-12-29 15:48:25 +0900

    activity move

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.myapplication.Activity.FreeActivity;
+import com.example.myapplication.Activity.ImgActivity;
+import com.example.myapplication.Activity.NumActivity;
+import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
     //branch 예제
@@ -18,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         numButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),NumActivity.class);
+                Intent intent=new Intent(getApplicationContext(), NumActivity.class);
                 startActivity(intent);
             }
         });
@@ -27,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         imgButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),ImgActivity.class);
+                Intent intent=new Intent(getApplicationContext(), ImgActivity.class);
                 startActivity(intent);
             }
         });
@@ -36,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         freeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),FreeActivity.class);
+                Intent intent=new Intent(getApplicationContext(), FreeActivity.class);
                 startActivity(intent);
             }
         });
